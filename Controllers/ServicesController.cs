cc6380bdd8c425903488dd0ccf028cdf589824ca
leandro-gomez-aparicio <84028006+leandro-gomez-aparicio@users.noreply.github.com>
2024-10-08 23:54:35 -0300

    Add files via upload

---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TurnosBack.Data.Contracts;
+using TurnosBack.Data.Models;
+
+namespace Entregable_5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : Controller
+    {
+        private readonly IServicesService _servicesService;
+
+        public ServicesController(IServicesService servicesService)
+        {
+            _servicesService = servicesService;
+        }
+
+
+        [HttpGet]
+        [Route("all")]
+        public IActionResult GetAllServices()
+        {
+            return Ok(_servicesService.GetAll());
+        }
+
+        [HttpGet]
+        [Route("filter")]
+        public IActionResult GetService(int precio_min, int precio_max)
+        {
+            try
+            {
+                var service = _servicesService.Get(precio_min, precio_max);
+
+                if (service == null)
+                {
+                    return NotFound("No hubo coincidencia");
+                }
+                else
+                {
+                    return Ok(service);
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "error");
+            }
+        }
+
+        [HttpPost]
+
+        public IActionResult CrearServicio([FromBody] TServicio servicio)
+        {
+            if (servicio != null) {
+                try
+                {
+                    _servicesService.Create(servicio);
+                    return Ok("Se agregó el servicio");
+                }
+                catch (Exception)
+                {
+                    return StatusCode(500, "Error");
+                    throw;
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+
+
+        [HttpPut]
+        [Route("edit/{id}")]
+
+        public IActionResult UpdateService(int id, [FromBody] TServicio servicioNuevo)
+        {
+            try
+            {
+                if (_servicesService.UpdateService(id, servicioNuevo))
+                {
+                    return Ok("Se ha actualizado el servicio.");
+                }
+                else
+                {
+                    return BadRequest("No se pudo");
+                }
+            }
+            catch (Exception exc)
+            {
+                return StatusCode(500, "error");
+                throw;
+            }
+        }
+
+        [HttpPut]
+        [Route("delete/{id}")]
+
+        public IActionResult DeleteService(int id)
+        {
+            try
+            {
+                if (_servicesService.Delete(id))
+                {
+                    return Ok("Se ha hecho la baja lógica");
+                }
+                else
+                {
+                    return BadRequest("No se pudo");
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Hubo un error");
+                throw;
+            }
+        }
+
+    }
+}
---
+++ Controllers/TurnoController.cs
@@ -0,0 +1,123 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using TurnosBack.Data.Contracts;
+using TurnosBack.Data.Models;
+
+namespace Entregable_5.Controllers
+{
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class TurnoController : Controller
+    {
+        private readonly ITurnoService _repository;
+
+        public TurnoController(ITurnoService repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public IActionResult CrearTurno([FromBody]TTurno turno)
+        {
+            if(turno == null)
+            {
+                return BadRequest("El turno no debe ser null");
+            }
+
+            try
+            {
+
+                if (_repository.CrearTurno(turno))
+                {
+                    return Ok("Se creó el turno");
+                }
+                else
+                {
+                    return BadRequest("No se pudo");
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Error");
+                throw;
+            }
+
+        }
+
+        [HttpGet]
+
+        public IActionResult ConsultarTurnos([FromQuery] string cliente)
+        {
+            if (string.IsNullOrEmpty(cliente))
+            {
+                return BadRequest("Cliente no puede estar vacío.");
+            }
+
+            var lista = _repository.ConsultarTurnos(cliente);
+
+            if(lista.Count > 0)
+            {
+                return Ok(lista);
+            }
+            else
+            {
+                return NotFound("No se encontraron coincidencias.");
+            }
+        }
+
+        [HttpPut]
+
+        public IActionResult EditarTurno([FromBody] TTurno turn)
+        {
+           if(turn == null)
+            {
+                return BadRequest("El turno debe estar completo.");
+            }
+
+            try
+            {
+                if (_repository.EditarTurno(turn))
+                {
+                    return Ok("Se editó el turno");
+                }
+                else
+                {
+                    return BadRequest("No se pudo");
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Error");
+                throw;
+            }
+        }
+
+        [HttpDelete]
+
+        public IActionResult CancelarTurno([FromQuery] int id) {
+            
+            if(id == null || id == 0)
+            {
+                return BadRequest("El id no puede ser vacio o 0");
+            }
+
+            try
+            {
+                if (_repository.EliminarTurno(id))
+                {
+                    return Ok("Se canceló el turno");
+                }
+                else
+                {
+                    return BadRequest("No se encontró el turno.");
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Error");
+                throw;
+            }
+        }
+    }
+}
